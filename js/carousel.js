c924037fde4631158d15441cdcc018857514c72d
Sam Elder <samelder925@gmail.com>
2022-09-18 12:00:26 -0400

    pop-up fix

--- js/carousel.js
+++ js/carousel.js
@@ -33,3 +33,86 @@
 // }
 
 // update();
+
+// HTML
+
+// <!-- reviews -->
+//             <!-- <section class="reviews">
+//                 <div class="review-title">What People Say</div>
+//                 <h2 class="header-medium">Trusted by Clients.</h2>
+//                 <div class="review-preview">
+//                     <img src="./assets/images/cube.png" alt="cube">
+//                 </div>
+
+//                 <div class="review-carousel">
+//                     <div class="review-item ">
+//                         <div class="review-text">
+//                             #1 folio Designer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//                         </div>
+//                         <div class="review-author">
+//                             <div class="avatar">
+//                                 <img src="./assets/images/ava-1.jpg" alt="avatar">
+//                             </div>
+//                             <div class="details">
+//                                 <div>Joe Schmoe</div>
+//                                 <div>Carp Corp</div>
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div class="review-item ">
+//                         <div class="review-text">
+//                             #2 folio Designer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//                         </div>
+//                         <div class="review-author">
+//                             <div class="avatar">
+//                                 <img src="./assets/images/ava-1.jpg" alt="avatar">
+//                             </div>
+//                             <div class="details">
+//                                 <div>Charlie Buckets</div>
+//                                 <div>Company.co</div>
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div class="review-item ">
+//                         <div class="review-text">
+//                             #3 folio Designer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//                         </div>
+//                         <div class="review-author">
+//                             <div class="avatar">
+//                                 <img src="./assets/images/ava-1.jpg" alt="avatar">
+//                             </div>
+//                             <div class="details">
+//                                 <div>John Jacob</div>
+//                                 <div>El El Sea LLC</div>
+//                             </div>
+//                         </div>
+//                     </div>
+
+//                     <div class="review-item">
+//                         <div class="review-text">
+//                             #4 folio Designer Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//                         </div>
+//                         <div class="review-author">
+//                             <div class="avatar">
+//                                 <img src="./assets/images/ava-1.jpg" alt="avatar">
+//                             </div>
+//                             <div class="details">
+//                                 <div>Duh Doe</div>
+//                                 <div>Simpson Services</div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+
+//                 <div class="slide-ctrl-container">
+//                     <button>
+//                         <i class="fas fa-arrow-left"></i>
+//                     </button>
+//                     <button>
+//                         <i class="fas fa-arrow-right"></i>
+//                     </button>
+//                 </div>
+
+//             </section> -->
